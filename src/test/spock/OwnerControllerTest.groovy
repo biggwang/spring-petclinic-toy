b926059e34da7f765ae9d5df194877f4bd017202
류윤광/플랫폼개바팀 <yungwang.ryu@musinsa.com>
2019-10-16 23:23:23 +0900

    first commit

---
+++ src/test/spock/OwnerControllerTest.groovy
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.owner
+
+import org.junit.runner.RunWith
+import org.springframework.test.context.junit4.SpringRunner
+import spock.lang.Unroll
+
+@RunWith(SpringRunner.class)
+class OwnerControllerTest extends Specification {
+
+    @Unroll
+    def "테스중입니다."() {
+
+    }
+
+}
