a7cfa4cdd357e9073581ecaa00abb2ca896f79ff
류윤광/플랫폼개바팀 <yungwang.ryu@musinsa.com>
2019-10-21 23:22:28 +0900

    테스트코드 작서중

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTest.groovy
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTest.groovy
@@ -1,8 +1,10 @@
 package org.springframework.samples.petclinic.owner
 
+import org.assertj.core.util.Lists
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.samples.petclinic.PetClinicApplication
@@ -12,8 +14,11 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import static org.mockito.BDDMockito.given
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view
 
 @SpringBootTest(classes = PetClinicApplication.class)
@@ -24,6 +29,20 @@ class OwnerControllerTest extends Specification {
     @Autowired
     private MockMvc mvc
 
+    @MockBean
+    private OwnerRepository owners
+
+    private Owner owner;
+
+    def setup() {
+        owner = new Owner();
+        owner.setId(1);
+        owner.setFirstName("류");
+        owner.setLastName("윤광");
+        owner.setAddress("윌스트리트 323");
+        owner.setCity("광명");
+        owner.setTelephone("6085551023");
+    }
 
     @Unroll
     def "주인 찾기 페이지 호출 테스트를 한다." () {
@@ -74,5 +93,31 @@ class OwnerControllerTest extends Specification {
                 .andExpect(view().name("owners/createOrUpdateOwnerForm"))
                 .andReturn()
                 .response
+    }
+
+    @Unroll
+    def "주인 마지막 이름이 존재하지 않을 경우 리스트 화면을 출력한다." () {
+
+        given:
+        given(this.owners.findByLastName("")).willReturn(Lists.newArrayList(owner, new Owner()));
+
+        expect:
+        mvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"));
+    }
+
+    @Unroll
+    def "주인 마지막 이름이 검색 되었을 경우 상세 페이지로 라다이렉트 된다." () {
+
+        given:
+        given(this.owners.findByLastName("윤광")).willReturn(Lists.newArrayList(owner));
+
+        expect:
+        mvc.perform(get("/owners")
+            .param("lastName", "윤광")
+        )
+            .andExpect(status().is3xxRedirection())
+            .andExpect(view().name("redirect:/owners/" + 1))
     }
 }
