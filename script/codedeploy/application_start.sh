ee92e42b06b4ff74045d4c03060b7b9a83ea3ccc
류윤광/플랫폼개바팀 <yungwang.ryu@musinsa.com>
2020-07-19 07:31:22 +0900

    codedeploy 스크립트 추가

---
+++ script/codedeploy/application_start.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/sh
+SERVICE_NAME=spring-petclnic
+SERVER_PORT=8080
+
+sudo systemctl daemon-reload
+service ${SERVICE_NAME} restart
+
+sleep 5 # for waitting stop daemon first
+
+CONTINUE=1
+
+while [ ${CONTINUE} -eq 1 ]
+do
+    sleep 1
+    PORT_STATUS=`netstat -an | grep LISTEN | grep ":${SERVER_PORT}" | wc -l`
+    if [ ${PORT_STATUS} -eq 1 ]
+    then
+        CONTINUE=0
+    fi
+done
+
