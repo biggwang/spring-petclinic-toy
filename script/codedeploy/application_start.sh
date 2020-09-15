a657ff74880fe23d31191f5285da640a8fa1017d
류윤광/플랫폼개바팀 <yungwang.ryu@musinsa.com>
2020-09-15 22:07:06 +0900

    배포테스트

--- script/codedeploy/application_start.sh
+++ script/codedeploy/application_start.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-SERVICE_NAME=spring-petclinic-2.1.6.RELEASE.jar
+SERVICE_NAME=spring-petclnic
 SERVER_PORT=8080
 
-nohup java -jar /home/ec2-user/build/$SERVICE_NAME
+sudo systemctl daemon-reload
+service ${SERVICE_NAME} restart
+
+sleep 5 # for waitting stop daemon first1
+
+CONTINUE=1
+
+while [ ${CONTINUE} -eq 1 ]
+do
+    sleep 1
+    PORT_STATUS=`netstat -an | grep LISTEN | grep ":${SERVER_PORT}" | wc -l`
+    if [ ${PORT_STATUS} -eq 1 ]
+    then
+        CONTINUE=0
+    fi
+done
+
