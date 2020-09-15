e3705b2e77450df4404190608711da8a656c4391
류윤광/플랫폼개바팀 <yungwang.ryu@musinsa.com>
2020-09-15 22:15:52 +0900

    배포테스트

--- script/codedeploy/application_start.sh
+++ script/codedeploy/application_start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-SERVICE_NAME=spring-petclinic-2.1.6.RELEASE.jar
-SERVER_PORT=8080
 
-nohup java -jar /home/ec2-user/build/${SERVICE_NAME}
+nohup java -jar /home/ec2-user/build/spring-petclinic-2.1.6.RELEASE.jar
